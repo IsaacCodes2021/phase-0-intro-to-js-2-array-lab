3c9fbda8f5022fd64851c7ffc4bb0fda7acfffa5
Isaac <isaac.businessiq@gmail.com>
2021-06-18 12:15:49 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+   name = [...cats.slice(), 'Broom'];
+   return name;
+}
+
+function prependCat(name) {
+    name = ['Arnold', ...cats.slice()]
+    return name;
+}
+
+function removeLastCat() {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = cats.slice(-2);
+    return newCats;
+}
+
+removeLastCat();
+console.log(cats);
